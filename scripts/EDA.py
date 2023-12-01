ecaef545297cfe22d240af36c20047f13d044573
Siddharth Grover <groversiddharth21@gmail.com>
2023-12-01 04:02:14 -0800

    function 2

--- scripts/EDA.py
+++ scripts/EDA.py
@@ -43,3 +43,42 @@ categorical_cols = ["job", "marital", "education", "default", "housing", "loan",
     print("\nLast 5 Rows:")
     display(data.tail(5))
 
+    """
+    Generate Exploratory Data Analysis (EDA) plots for visualizing both categorical and
+    numerical features in the given df.
+
+    Parameters:
+    - data (DataFrame): The input DataFrame containing the data for analysis.
+    - numeric_cols (list): List of numerical columns for EDA plots.
+    - categorical_cols (list): List of categorical columns for EDA plots.
+      Default for numeric_cols or categorical_cols is an empty list.
+
+    Returns:
+    - Two plots: The first element is the Altair chart for categorical features, 
+    and the second element is the Altair chart for numerical features.
+    """
+    
+    if len(categorical_cols) != 0:
+        categorical_plot = (
+            alt.Chart(data)
+            .mark_bar(opacity=0.9)
+            .encode(
+                y=alt.Y(alt.repeat()).type("nominal").sort("-x"),
+                x=alt.X("count()", title="Count").stack(False),
+            )
+            .repeat(categorical_cols, columns=2)
+        )
+
+    if len(numeric_cols) != 0:
+        numeric_plot = (
+            alt.Chart(data)
+            .mark_bar(opacity=0.9)
+            .encode(
+                y=alt.Y(alt.repeat()).type("quantitative").bin(maxbins=25).sort("-x"),
+                x=alt.X("count()", title = "Count").stack(False),
+            )
+            .repeat(numeric_cols, columns=2)
+        )
+    #SAVE PLOTS AS .PNG
+    categorical_plot.save(os.path.join(plot_to, "categorical_dist_by_feat.png"))
+    numerical_plot.save(os.path.join(plot_to, "numerical_dist_by_feat.png"))
